ffb6bf7ed9aa37506dc6c111f9ebc83b25baac1f
RyanLee9219 <the9219@gmail.com>
2024-03-20 23:27:07 -0400

    1st error handling practice2

--- Practice/error_handling/errors.py
+++ Practice/error_handling/errors.py
@@ -4,11 +4,17 @@ while True:
     try:
         age = int(input('what is your age? '))
         10/age
+        raise Exception('hey cut it out')
         # print(age)
-    except ValueError:
-        print('please enter a number')
+    # except ValueError:
+    #     print('please enter a number')
+    #     continue
     except ZeroDivisionError:
         print('please enter age higher than 0')
+        break
     else:
         print('thanks!')
         break
+    finally:
+        print('ok, I am finally done')
+    print('can you hear me')
